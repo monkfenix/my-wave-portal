05a1320fff142af41bb7a75df6df4e4c029ea5a9
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-11-04 17:59:49 +1000

    add deployment and run scripts and message contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,38 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
+
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("I AM SMART CONTRACT. POG");
+    }
+
+    function wave(string memory _message) public {
+        totalWaves += 1;
+        console.log("%s has waved with message %s!", msg.sender, _message);
+
+        // Add the wave data to the array
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
