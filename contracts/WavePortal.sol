4aa7692bad3d03f58056626cc1fbf50f2588aa0d
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-11-04 23:16:21 +1000

    make it payable

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -17,8 +17,8 @@ contract WavePortal {
 
     Wave[] waves;
 
-    constructor() {
-        console.log("I AM SMART CONTRACT. POG");
+    constructor() payable {
+        console.log("We have been constucted to enable payments. POG");
     }
 
     function wave(string memory _message) public {
@@ -29,6 +29,14 @@ contract WavePortal {
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,19 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
 
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
+
+    /*
+    * Get contract balance
+    */
+    let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance));
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
